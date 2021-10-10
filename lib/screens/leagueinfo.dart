dded6c383c905a4d637498f6642143ef4165821d
omisan01 <omkar12mitake@gmail.com>
2021-10-10 15:48:10 +0530

    edited

--- lib/screens/leagueinfo.dart
+++ lib/screens/leagueinfo.dart
@@ -1,2 +1,12 @@
 import 'package:flutter/material.dart';
 
+class  LeagueInfo extends StatelessWidget {
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
